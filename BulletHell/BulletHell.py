d6e916ae51717fc9055274154ffc72a2821656f5
Jurgen Heysen <jheysen@gmail.com>
2013-08-21 19:46:55 -0400

    fibonnacci

--- BulletHell/BulletHell.py
+++ BulletHell/BulletHell.py
@@ -1,3 +1,13 @@
 print('Hello World')
 print('asdf')
 print('mensaje')
+
+def fib(n):
+    a,b = 0,1
+    while a<n:
+        print a,
+        a,b = b,a+b
+
+num = int(raw_input('Introduzca un número: '))
+fib(num)
+print('')
